b76c774c1fca1a05570b4ad10d9d96be2d888ec2
Vladimir Kamuz <v.kamuz@gmail.com>
2018-03-21 23:16:42 +0200

    Init

---
+++ js/main.js
@@ -0,0 +1,26 @@
+function openNav() {
+    document.getElementById("mySidenav").style.left = "0";
+    // document.getElementById("main").style.marginLeft = "250px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.left = "-250px";
+    // document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+        this.classList.toggle("fa-caret-up");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
